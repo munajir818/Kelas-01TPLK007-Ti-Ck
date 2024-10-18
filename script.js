61dd1444731f119ebb9e28720c7d5e62ae48a0a1
munajir818 <151390704+munajir818@users.noreply.github.com>
2024-10-18 17:43:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Implementasi logika login di sini
+    alert('Login berhasil!');
+});
